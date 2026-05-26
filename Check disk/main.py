0135d7d353ae613c7aac4fda910337b0be0ea1aa
Abdelaziz shahin <abd91business@gmail.com>
2026-05-27 00:20:53 +0200

    adding comments to the code for explanation

--- Check disk/main.py
+++ Check disk/main.py
@@ -14,10 +14,12 @@ COLOR_BOLD = "\033[1m"
 COLOR_RESET = "\033[0m"
 
 def check_disk_space():
+    # Fetch total, used, and free space in bytes
     total,used,free=shutil.disk_usage(DISK_PATH)
 
+    # Calculate the percentage of free space left
     free_percent = (free/total)*100
-
+    # Convert bytes to Gigabytes (GB) for human-readable logs
     bytes_in_gb = 1024 ** 3
     free_gb = free / bytes_in_gb
     total_gb = total / bytes_in_gb
