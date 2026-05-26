f042b6fb4906988adb73789f7f8e11b1757397cd
Abdelaziz shahin <abd91business@gmail.com>
2026-05-27 00:17:20 +0200

    creating method to count convert bytes in giga bytes

--- Check disk/main.py
+++ Check disk/main.py
@@ -1,3 +1,23 @@
 import shutil
 import datetime
 import sys
+
+# configuration
+DISK_PATH ='c:\\'
+DISK_THRESHOLD_PERCENT= 15 # alert if the free space drop below this percentage
+LOG_FILE="disk_alerts.log"
+
+# terminal color
+COLOR_GREEN = "\033[92m"
+COLOR_RED = "\033[91m"
+COLOR_BOLD = "\033[1m"
+COLOR_RESET = "\033[0m"
+
+def check_disk_space():
+    total,used,free=shutil.disk_usage(DISK_PATH)
+
+    free_percent = (free/total)*100
+
+    bytes_in_gb = 1024 ** 3
+    free_gb = free / bytes_in_gb
+    total_gb = total / bytes_in_gb
